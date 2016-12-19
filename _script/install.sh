7e15673069edb1dac6a48a4fff5cf7af095a7834
Barrett Griffith <mrbarrettgriffith@gmail.com>
2016-12-19 10:55:19 -0500

    Update deploy

---
+++ _script/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x # Show the output of the following commands (useful for debugging)
+    
+# Import the SSH deployment key
+openssl aes-256-cbc -K $encrypted_6a97e7e82599_key -iv $encrypted_6a97e7e82599_iv -in travis_deploy.enc -out travis_deploy -d
+rm travis_deploy.enc # Don't need it anymore
+chmod 600 travis_deploy
+mv travis_deploy ~/.ssh/id_rsa
+    
+# Install zopfli
+#git clone https://code.google.com/p/zopfli/
+#cd zopfli
+#make
+#chmod +x zopfli
+#cd ..
