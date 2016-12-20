0cdf59a17f4cb3b60a4d58f944cf5abac65fd452
Barrett Griffith <mrbarrettgriffith@gmail.com>
2016-12-20 12:37:52 -0500

    try different approach to define IV

--- _script/install.sh
+++ _script/install.sh
@@ -2,6 +2,11 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis_deploy.enc -out travis_deploy -d
 rm travis_deploy.enc # Don't need it anymore
 chmod 600 travis_deploy
 echo "Host samarashuttle.cr" >> ~/.ssh/config
