475d958a04abe19be1672049edeac5efc02e2d40
Barrett Griffith <mrbarrettgriffith@gmail.com>
2016-12-21 20:02:56 -0500

    Cleanup and rekey

--- _script/deploy.sh
+++ _script/deploy.sh
@@ -6,14 +6,13 @@ if [ $TRAVIS_BRANCH == 'gh-pages' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
     cd _site
     git init
-        
     git remote add deploy "deploy@146.185.156.190:/home/deploy/SamaraShuttle"
     git config user.name "travis_deploy@samarashuttle.cr"
     git config user.email "travis_deploy@samarashuttle.cr"
     
     git add .
     git commit -m "Deploy"
-    git push --force deploy gh-pages
+    git push --force deploy master
 else
     echo "Not deploying, since this branch isn't gh-pages."
 fi
--- _script/install.sh
+++ _script/install.sh
@@ -7,14 +7,12 @@ ENCRYPTED_IV_VAR="$encrypted_${ENCRYPTION_LABEL}_iv"
 openssl aes-256-cbc -K $encrypted_6a97e7e82599_key -iv $encrypted_6a97e7e82599_iv -in travis_deploy.enc -out travis_deploy -d
 rm travis_deploy.enc # Don't need it anymore
 chmod 600 travis_deploy
-echo "Host samarashuttle.cr" >> ~/.ssh/config
-echo "  IdentityFile ~/.ssh/travis_deploy" >> ~/.ssh/config
+#echo "Host samarashuttle.cr" >> ~/.ssh/config
+#echo "  IdentityFile ~/.ssh/travis_deploy" >> ~/.ssh/config
 mv travis_deploy ~/.ssh/travis_deploy
-cat ~/.ssh/travis_deploy
-ssh-keygen -y -f ~/.ssh/travis_deploy  -P "" > ~/.ssh/travis_deploy.pub    
-sed '$s/$/ travis_deploy@samarashuttle.cr/' ~/.ssh/travis_deploy.pub > ~/.ssh/travis_deploy.pub.temp  && mv -- ~/.ssh/travis_deploy.pub.temp ~/.ssh/travis_deploy.pub.temp
-cat ~/.ssh/travis_deploy.pub
-ls ~/.ssh
+#ssh-keygen -y -f ~/.ssh/travis_deploy  -P "" > ~/.ssh/travis_deploy.pub    
+#sed '$s/$/ travis_deploy@samarashuttle.cr/' ~/.ssh/travis_deploy.pub > ~/.ssh/travis_deploy.pub.temp  && mv -- ~/.ssh/travis_deploy.pub.temp ~/.ssh/travis_deploy.pub.temp
+#ls ~/.ssh
 # Install zopfli
 #git clone https://code.google.com/p/zopfli/
 #cd zopfli
